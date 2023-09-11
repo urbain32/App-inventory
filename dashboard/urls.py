73ed82da08d9dddeea6930e269b3ec7dcea40824
urbain32 <urbainq32@gmail.com>
2023-09-11 15:50:35 +0200

    creating the app in another repo

---
+++ dashboard/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# for our function in views to be displayed we must import them 
+from . import views
+
+# calling our function in views
+# path(giving url path name,name of the function in views,name we want to give our function)
+urlpatterns=[
+    path('',views.index,name='dashboard-index'),
+]
