d9d63c35f34f44fb3a63244ab48163662cdd7015
urbain32 <urbainq32@gmail.com>
2023-09-22 15:30:34 +0200

    registering category in our models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models.product import Product
+from .models.category import Category
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Category)
