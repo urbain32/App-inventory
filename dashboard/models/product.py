accee39e5f99cab9e1337bc355e082132ed1b457
urbain32 <urbainq32@gmail.com>
2023-09-22 11:35:12 +0200

    the error of importing category

--- dashboard/models/product.py
+++ dashboard/models/product.py
@@ -1,9 +1,9 @@
 from django.db import models
-from dashboard.models import category
+from .category import Category
 
 class Product(models.Model):
     name=models.CharField(max_length=25,null=False,unique=True)
-    category = models.ForeignKey(category,on_delete=models.PROTECT) 
+    category = models.ForeignKey(Category,on_delete=models.PROTECT) 
     quantity = models.PositiveIntegerField(null=True)
     def __str__(self):
         return self.name
