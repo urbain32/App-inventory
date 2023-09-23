dec0c6e91467cb531bc9eb1e15d8f2c4e172477e
urbain32 <urbainq32@gmail.com>
2023-09-23 12:21:39 +0200

    creating views

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,6 +8,8 @@ urlpatterns=[
     path('',views.index,name='dashboard-index'),
     path('staff/',views.staff,name='dashboard-staff'),
     path('product/',views.product,name='dashboard-product'),
-    path('category/',views.category,name='dashboard-category'),
     path('order/',views.order,name='dashboard-order'),
+    path('category/',views.categoryView,name='dashboard-category'),
+    path('categoryAdd/',views.categoryAddView,name='create_Category'),
+
 ]
