47a8528f9297631d08ffe50a2b7261f9dbe62e51
urbain32 <urbainq32@gmail.com>
2023-09-25 11:20:51 +0200

    renaming the html folder

--- dashboard/views/dashboard.py
+++ dashboard/views/dashboard.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'dashboard/index.html')
+    return render(request,'dashboard/home_page/index.html')
 
