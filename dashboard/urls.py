23a6821af974deab17585391e3b397a1578a8130
urbain32 <urbainq32@gmail.com>
2023-09-25 11:18:31 +0200

    separating the urls for the separated views

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 # for our function in views to be displayed we must import them 
-from . import views
+from .views import dashboard,staff,product,order,category
 
 # calling our function in views
 # path(giving url path name,name of the function in views,name we want to give our function)
 urlpatterns=[
-    path('',views.index,name='dashboard-index'),
-    path('staff/',views.staff,name='dashboard-staff'),
-    path('product/',views.product,name='dashboard-product'),
-    path('order/',views.order,name='dashboard-order'),
-    path('category/',views.categoryView,name='dashboard-category'),
-    path('categoryAdd/',views.categoryAddView,name='create_Category'),
+    path('',dashboard.index,name='dashboard-index'),
+    path('staff/',staff.staff,name='dashboard-staff'),
+    path('product/',product.product,name='dashboard-product'),
+    path('order/',order.order,name='dashboard-order'),
+    path('category/',category.categoryView,name='dashboard-category'),
+    path('categoryAdd/',category.categoryAddView,name='create_Category'),
 
 ]
