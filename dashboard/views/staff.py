7418c12afad61b9f78d856d80380a487aa4e60f1
urbain32 <urbainq32@gmail.com>
2023-09-25 11:22:59 +0200

    relocating staff template

--- dashboard/views/staff.py
+++ dashboard/views/staff.py
@@ -2,5 +2,5 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 def staff(request):
-    return render(request,'dashboard/staff.html')
+    return render(request,'dashboard/staff/staff.html')
 
