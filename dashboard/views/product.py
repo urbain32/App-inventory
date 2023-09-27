f77d018788c44f82f37e7af82d4c3a8338d7c25a
urbain32 <urbainq32@gmail.com>
2023-09-27 15:33:06 +0200

    creating the view to add product

--- dashboard/views/product.py
+++ dashboard/views/product.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from ..models import Product,Category
+from ..forms import ProductForm
 
 # Create your views here.
 
@@ -8,4 +9,10 @@ def productView(request):
     products = Product.objects.all() 
     categories = Category.objects.all() 
     return render(request, 'dashboard/product/index.html', {'products': products,'categories':categories})
+def productAddView(request):
+        if request.method == 'POST':
+            form = ProductForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/product')
 
