c3b0520f3c61fe58c0b9b6f238eb0e427e8f2ad9
urbain32 <urbainq32@gmail.com>
2023-09-27 15:14:09 +0200

    adding the get for category

--- dashboard/views/product.py
+++ dashboard/views/product.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
-from ..models import Product
+from ..models import Product,Category
 
 # Create your views here.
 
 # product
 def productView(request):
     products = Product.objects.all() 
-    return render(request, 'dashboard/product/index.html', {'products': products})
+    categories = Category.objects.all() 
+    return render(request, 'dashboard/product/index.html', {'products': products,'categories':categories})
 
