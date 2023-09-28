240c70f4c3a5f566b670db87365e23d4e6d48549
urbain32 <urbainq32@gmail.com>
2023-09-28 09:28:58 +0200

    the product admin will help to display in the admin name and quantity

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models.product import Product
 from .models.category import Category
+
+# the product admin will help to display in the admin name and quantity 
+class ProductAdmin (admin.ModelAdmin):
+    list_display=('name','category','quantity')
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Category)
