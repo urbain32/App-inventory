8b215883bd33a1c7c4c4b0cf60c509cc90e44551
urbain32 <urbainq32@gmail.com>
2023-09-28 09:35:32 +0200

    when we filter we must use , at the end to say it is turple else we use []to say is a list

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -5,6 +5,8 @@ from .models.category import Category
 # the product admin will help to display in the admin name and quantity 
 class ProductAdmin (admin.ModelAdmin):
     list_display=('name','category','quantity')
+    # when we filter we must use , at the end to say it is turple else we use []to say is a list
+    list_filter=('category',)
 # Register your models here.
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Category)
