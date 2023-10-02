c4e60f682cc5a243053165cd84dc098a58971a7d
urbain32 <urbainq32@gmail.com>
2023-10-02 15:52:40 +0200

    adding a class meta to product

--- dashboard/models/product.py
+++ dashboard/models/product.py
@@ -5,5 +5,7 @@ class Product(models.Model):
     name=models.CharField(max_length=25,null=False,unique=True)
     category = models.ForeignKey(Category,on_delete=models.PROTECT) 
     quantity = models.PositiveIntegerField(null=True)
+    class Meta:
+        verbose_name_plural = 'Product' # tin our admin the name will be Product not productsa
     def __str__(self):
         return f'{self.name}-{self.quantity}'
