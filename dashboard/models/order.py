cc64e726b506b7e43a0ac207fe62db24498ebfbe
urbain32 <urbainq32@gmail.com>
2023-10-02 15:38:03 +0200

    from django we are using User of django as foreign

--- dashboard/models/order.py
+++ dashboard/models/order.py
@@ -1,6 +1,8 @@
 from django.db import models
 from .product import  Product
+from django.contrib.auth.models import User
 
 class Order(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,null=True) 
-    staff = models.ForeignKey(models.CASCADE,null=True) 
+    staff = models.ForeignKey(User,models.CASCADE,null=True) 
+    
