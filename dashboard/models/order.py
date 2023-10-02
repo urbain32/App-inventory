25cfd5475eff9a5ad02e169521ef54b4d896b909
urbain32 <urbainq32@gmail.com>
2023-10-02 15:53:11 +0200

    adding a class meta

--- dashboard/models/order.py
+++ dashboard/models/order.py
@@ -7,6 +7,8 @@ class Order(models.Model):
     staff = models.ForeignKey(User,models.CASCADE,null=True) 
     order_quantity=models.PositiveIntegerField(null=True)
     date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'Order'
     def __str__(self):
         return f'{self.product} orderd by {self.staff.username}'
 
