e2afbe837f7d452383e95828eba5c14cccd87373
urbain32 <urbainq32@gmail.com>
2023-10-02 15:53:39 +0200

    adding a class meta

--- dashboard/models/category.py
+++ dashboard/models/category.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=25,unique=True,null=False)
+    class Meta:
+        verbose_name_plural = 'Category'
     def __str__(self) :
         return self.name
