a1f08af19dde3c9f39b52668b9ff0dddb040209e
urbain32 <urbainq32@gmail.com>
2023-10-02 15:41:33 +0200

    adding the date

--- dashboard/models/order.py
+++ dashboard/models/order.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Order(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,null=True) 
     staff = models.ForeignKey(User,models.CASCADE,null=True) 
-    quantity_order=models.PositiveIntegerField(null=True)
+    order_quantity=models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=Truerue)
 
 
     
