d8b8febc95f18e72346394ff94b02d4cccb9367b
urbain32 <urbainq32@gmail.com>
2023-10-02 15:48:18 +0200

    in admin registering our order model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models.product import Product
+from .models.order import Order
 from .models.category import Category
 
 # the product admin will help to display in the admin name and quantity 
@@ -10,3 +11,4 @@ class ProductAdmin (admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Category)
+admin.site.register(Order)
