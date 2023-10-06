a06b9830d58688d7092f2857c6812f77736b51e8
urbain32 <urbainq32@gmail.com>
2023-10-06 16:18:36 +0200

    register view creating it url in the project url

--- inventoryApp/urls.py
+++ inventoryApp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from user.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
+    path('register/',register.registerView,name='user-register')
 ]
