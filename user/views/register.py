939f662ae603e26ff9eacc53ef9520e388db89aa
urbain32 <urbainq32@gmail.com>
2023-10-10 16:03:37 +0200

    now calling the  createuserform in views

--- user/views/register.py
+++ user/views/register.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from ..forms.RegisterForm import CreateUserForm
 
 # Create your views here.
 
 # Category
 def registerView(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('user-register')
     else:
-        form = UserCreationForm()
+        form = CreateUserForm()
     context={'form':form}
     return render(request, 'user/register.html',context)
