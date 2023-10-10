14c544b49bfa72a06c4c3e1deb6d9e525c4d1db0
urbain32 <urbainq32@gmail.com>
2023-10-10 15:58:25 +0200

    creating a meata class that take all the fields

--- user/forms/RegisterForm.py
+++ user/forms/RegisterForm.py
@@ -4,6 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 class CreateUserForm(UserCreationForm):
     email=forms.EmailField()
-    
+
+    class Meta:
+        model=User
+        # the __all__ will bring all the field int UserCreationForm
+        fields='__all__'
+
 
         
