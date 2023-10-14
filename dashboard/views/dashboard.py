a7f7cafd4e3cc550b0fa2c7dbb9ebfdb2861d516
urbain32 <urbainq32@gmail.com>
2023-10-14 12:47:49 +0200

    using login required from django decorators

--- dashboard/views/dashboard.py
+++ dashboard/views/dashboard.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
 
 def index(request):
     return render(request,'dashboard/home_page/index.html')
