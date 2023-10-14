664d9439224514a0ec43011241c31d69beb94d33
urbain32 <urbainq32@gmail.com>
2023-10-14 13:00:48 +0200

    removing login_url coz is  declared in settings

--- dashboard/views/staff.py
+++ dashboard/views/staff.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='user-login')
+@login_required
 def staff(request):
     return render(request,'dashboard/staff/staff.html')
 
