48e9596cb22513f41dd37ff4a49c581583c16cef
urbain32 <urbainq32@gmail.com>
2023-10-14 12:53:01 +0200

    adding login required

--- dashboard/views/staff.py
+++ dashboard/views/staff.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required(login_url='user-login')
 def staff(request):
     return render(request,'dashboard/staff/staff.html')
 
