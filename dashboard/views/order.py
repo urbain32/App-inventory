2eee0b58536ff51272964394b3cdca4bc8ebaf8c
urbain32 <urbainq32@gmail.com>
2023-10-14 12:52:51 +0200

    adding login required

--- dashboard/views/order.py
+++ dashboard/views/order.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+# Create your views here.
+@login_required(login_url='user-login')
 def order(request):
     return render(request,'dashboard/order/order.html')
