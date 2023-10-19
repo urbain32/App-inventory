8e24317a8a94dbc580b6b0667b036bb06dc53212
urbain32 <urbainq32@gmail.com>
2023-10-19 15:46:26 +0200

    adding the field for phone

--- user/models.py
+++ user/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     staff= models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     address=models.CharField(max_length=200,null=True)
+    pone=models.CharField(max_length=20,null=True)
