e5b2792c2abb6c662e99e956394c602cd286b0df
urbain32 <urbainq32@gmail.com>
2023-10-19 15:45:56 +0200

    adding the field for address

--- user/models.py
+++ user/models.py
@@ -3,3 +3,4 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     staff= models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    address=models.CharField(max_length=200,null=True)
