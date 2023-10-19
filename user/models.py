7d165af976bdddd41a8a2c60ccb9f0c6975f56ba
urbain32 <urbainq32@gmail.com>
2023-10-19 15:48:37 +0200

    adding the field for uploading images

--- user/models.py
+++ user/models.py
@@ -5,3 +5,4 @@ class Profile(models.Model):
     staff= models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     address=models.CharField(max_length=200,null=True)
     phone=models.CharField(max_length=20,null=True)
+    image=models.ImageField(default='avatar.jpg',upload_to='Profile_Images')
