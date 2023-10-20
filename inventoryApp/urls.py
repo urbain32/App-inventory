f9ef08d3501ed612fc8682351d83cabe3a121133
urbain32 <urbainq32@gmail.com>
2023-10-20 15:40:23 +0200

    the url to add to help in uploading media

--- inventoryApp/urls.py
+++ inventoryApp/urls.py
@@ -4,6 +4,8 @@ from django.urls import path,include
 from user.views import register,profile
 # using the built in login logout of django
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +14,4 @@ urlpatterns = [
     path('profile/',profile.profile,name='user-profile'),
     path('',auth_views.LoginView.as_view(template_name='user/login.html'),name='user-login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name='user-logout')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
