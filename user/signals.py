05cb73ea41adcc617eea73d4360e09d5c082d1a6
urbain32 <urbainq32@gmail.com>
2023-10-26 15:38:10 +0200

    using the receiver

--- user/signals.py
+++ user/signals.py
@@ -2,3 +2,5 @@ from django.contrib.auth.models import User
 from .models import Profile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+@receiver(post_save, sender=User )
