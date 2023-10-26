45d7070a65213c26130d391e31a918bd42fb7ae2
urbain32 <urbainq32@gmail.com>
2023-10-26 15:44:11 +0200

    create profile will help us to create profile for staff created

--- user/signals.py
+++ user/signals.py
@@ -4,4 +4,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
-def create_profile():
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(staff=instance)
