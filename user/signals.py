72343e1cbf19103ff953664fd3e85c093475a38a
urbain32 <urbainq32@gmail.com>
2023-10-26 15:39:08 +0200

    creating a function for signals

--- user/signals.py
+++ user/signals.py
@@ -3,4 +3,5 @@ from .models import Profile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-@receiver(post_save, sender=User )
+@receiver(post_save, sender=User)
+def create_profile():
