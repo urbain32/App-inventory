65cdd85a3acde78711532c7b55cb743183e290d1
urbain32 <urbainq32@gmail.com>
2023-10-30 16:22:20 +0200

    adding a context

--- user/views/profile.py
+++ user/views/profile.py
@@ -7,4 +7,7 @@ def profile(request):
     return render(request, 'user/profile.html')
 
 def profile_update(request):
+    context={
+        
+    }
     return render(request, 'user/profile_update.html')
