0825c316ae19387d7266ce53746507f62af0cb54
urbain32 <urbainq32@gmail.com>
2023-10-31 15:45:00 +0200

    url to update the profile

--- inventoryApp/urls.py
+++ inventoryApp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', include('dashboard.urls')),
     path('register/',register.registerView,name='user-register'),
     path('profile/',profile.profile,name='user-profile'),
+    path('profile/update/',profile.profile_update,name='user-profile-update'),
     path('',auth_views.LoginView.as_view(template_name='user/login.html'),name='user-login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'),name='user-logout')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
