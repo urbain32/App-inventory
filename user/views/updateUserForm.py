4b69c72367a37a9d4ba93c600d8de676eb534499
urbain32 <urbainq32@gmail.com>
2023-11-03 15:42:31 +0200

    the form to uopdate our user form

--- user/views/updateUserForm.py
+++ user/views/updateUserForm.py
@@ -1,8 +1,2 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
-from ..forms.RegisterForm import CreateUserForm
-from ..forms.UpdateUserForm import UpdateUserForm,ProfileUpdateForm
 
-# Create your views here.
 
-# Category
